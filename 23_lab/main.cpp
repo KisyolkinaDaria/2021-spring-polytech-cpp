dbe157dba4e4d9a1fc94c1cf6d361558fb176240
KisyolkinaDaria <dkiselkinad@gmail.com>
2021-04-05 17:14:13 +0300

    enter new array

--- 23_lab/main.cpp
+++ 23_lab/main.cpp
@@ -1,10 +1,41 @@
-// Напишите функцию, которая заполняет массив случайными
+﻿// Напишите функцию, которая заполняет массив случайными
 // целыми числами из диапазона от 0 до n. Воспользуйтесь функциями
 // std::rand() и std::srand().
 
+#include <iostream>
 #include <cstdlib>
+#include <ctime>
+#include <clocale>
+using namespace std;
 
-int main() {
-    // Решение тут
-    return 0;
+void array_initialization(int array_dimension, int n)
+{
+	int array[2][2];
+	srand(time(0));
+
+	for (int i = 0; i < array_dimension; i++)
+	{
+		for (int j = 0; j < array_dimension; j++)
+		{
+			array[i][j] = rand() % n;
+
+			cout << array[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	setlocale(LC_CTYPE, "RUS");
+	
+	int array_dimension;
+	int n;
+	cout << "Введите размерность массива:" << endl;
+	cin >> array_dimension;
+	cout << "Введите конечное число рандомных чисел:" << endl;
+	cin >> n;
+	cout << endl;
+	array_initialization(array_dimension, n);
+
 }
